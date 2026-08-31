3b42e70776156243fd4a9797ed1a958adb9db389
hugoVHalves <129858165+hugoVHalves@users.noreply.github.com>
2026-08-31 17:24:31 -0300

    a

---
+++ Assets/Script/Lixeira.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class Lixeira : MonoBehaviour
+{
+    public TipoLixo lixo;
+}
---
+++ Assets/Script/TipoLixo.cs
@@ -0,0 +1,9 @@
+public enum TipoLixo
+{
+    Azul,
+    Amarelo,
+    Cinza,
+    Vermelho,
+    Verde,
+    Laranja
+}
